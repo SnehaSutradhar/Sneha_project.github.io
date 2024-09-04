9acde27a7a26ac469b7bdbdde92c2bc2defdd6d6
SnehaSutradhar <151703360+SnehaSutradhar@users.noreply.github.com>
2024-09-04 23:20:53 +0530

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,23 @@
+// 3line menu
+
+function myFunction(x) {
+    x.classList.toggle("change");
+  }
+  
+  // 3line menu
+
+  
+  var acc = document.getElementsByClassName("click");
+  var i;
+  
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      } 
+    });
+  }
---
+++ slider.js
@@ -0,0 +1,38 @@
+
+
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+const slider = document.querySelector('.slider');
+
+const images = document.querySelectorAll('.image');
+
+let slideNumber = 1;
+const length = images.length;
+
+const nextSlide = () => {
+    slider.style.transform = `translateX(-${slideNumber * 1000}px)`;
+    slideNumber++; 
+};
+
+const prevSlide = () => {
+    slider.style.transform = `translateX(-${(slideNumber - 2) * 1000}px)`;
+    slideNumber--; 
+};
+
+const getFirstSlide = () => {
+    slider.style.transform = 'translateX(0px)';
+    slideNumber = 1; 
+};
+
+const getLastSlide = () => {
+    slider.style.transform = `translateX(-${(length - 1) * 1000}px)`;
+    slideNumber = length; 
+};
+
+right.addEventListener('click', () => {
+    slideNumber < length ? nextSlide() : getFirstSlide();  
+});
+
+left.addEventListener('click', () => {
+    slideNumber > 1 ? prevSlide() : getLastSlide();  
+});
